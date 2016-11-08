af26dd6337a199ffac0290effb215f29e19de0d5
ioana-mot <mot.ioana@yahoo.com>
2016-11-08 17:33:33 +0200

    New project

---
+++ src/main/java/servlets/Test2Servlet.java
@@ -0,0 +1,21 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Test2Servlet
+ */
+public class Test2Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().print("Hello from Java - Flori");
+	}
+
+	
+
+}
